41bd26c914a6af6ca8f91cc00b82ddc565f9b7d8
Alex Pratheep <alexpratheep@live.com>
2026-02-19 12:41:20 +0530

    Create storage accounts via module for_each

--- provider.tf
+++ provider.tf
@@ -11,10 +11,6 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "8581ca28-6790-4e30-b862-6034d027aa12"
-  client_secret   = "0Co8Q~nsk8YLdXdIMAF6vF2Nphu1CPkgmHW.nbfs"
-  tenant_id       = "9729e73e-b2fc-4148-94e5-b6e7a0016dce"
-  subscription_id = "8b0f87f9-f3b6-46fd-ad58-98654b93d97f"
 }
 
 
