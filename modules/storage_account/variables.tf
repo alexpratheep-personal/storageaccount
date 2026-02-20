1ba5a982d554cca3f00a5bdc96a047d41cb8debc
Alex Pratheep <alexpratheep@live.com>
2026-02-20 18:45:44 +0530

    ml

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -1,10 +1,9 @@
-variable "sa" {
-  type = object({
+variable "storage_accounts" {
+  type = map(object({
     name                     = string
-    resource_group_name      = string
-    location                 = string
+    rg_key                   = string
     account_tier             = string
     account_replication_type = string
     tags                     = optional(map(string), {})
-  })
-}
+  }))
+}s
