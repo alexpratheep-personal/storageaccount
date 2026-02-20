34fae4bc757c07bad4dfa73f5f133da78200050c
Alex Pratheep <alexpratheep@live.com>
2026-02-20 19:37:10 +0530

    ol

--- modules/resource_group/output.tf
+++ modules/resource_group/output.tf
@@ -1,14 +1,14 @@
-output "id" {
+output "rg_id" {
   description = "Resource Group resource ID"
-  value       = azurerm_resource_group.this.id
+  value       = azurerm_resource_group.rg.id
 }
 
-output "name" {
+output "rg_name" {
   description = "Resource Group name"
-  value       = azurerm_resource_group.this.name
+  value       = azurerm_resource_group.rg.name
 }
 
-output "location" {
+output "rg_location" {
   description = "Resource Group location"
-  value       = azurerm_resource_group.this.location
+  value       = azurerm_resource_group.rg.location
 }
